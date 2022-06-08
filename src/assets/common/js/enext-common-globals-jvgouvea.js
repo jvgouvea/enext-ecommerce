8b1a28b557a8a76ef07e996358d4eac1b2405c08
jvgouvea <contato.jvgouvea@gmail.com>
2022-06-08 01:22:59 -0300

    add vitrine

--- src/assets/common/js/enext-common-globals-jvgouvea.js
+++ src/assets/common/js/enext-common-globals-jvgouvea.js
@@ -1,1 +1,27 @@
-console.log("foo");
+$(function () {
+  $(".vitrine ul").slick({
+    infinite: true,
+    slidesToShow: 4,
+    SlidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
